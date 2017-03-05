30ac05127d416045f6c449dd4099a12e37bf606d
Mike de Libero <medelibero@gmail.com>
2017-03-04 21:02:04 -0800

    2.3.6 release
    * Added function exist checks for the nonce functions to avoid conflicts

--- external-libs/wp-simple-nonce/wp-simple-nonce.php
+++ external-libs/wp-simple-nonce/wp-simple-nonce.php
@@ -15,18 +15,20 @@ if ( ! class_exists( 'WPSimpleNonce' ) ) {
 
 add_shortcode( 'simpleNonce', 'WPSimpleNonce::createNonce' );
 
-
-function wp_snonce_cleanup() {
-	WPSimpleNonce::clearNonces();
-}
-
-add_action( 'wp_simple_nonce_cleanup', 'wp_snonce_cleanup' );
-
-
-function wp_simple_nonce_register_garbage_collection() {
-	if ( ! wp_next_scheduled( 'wp_simple_nonce_cleanup' ) ) {
-		wp_schedule_event( time(), 'daily', 'wp_simple_nonce_cleanup' );
+if( function_exists( 'wp_snonce_cleanup' ) ) {
+	function wp_snonce_cleanup() {
+		WPSimpleNonce::clearNonces();
 	}
+
+	add_action( 'wp_simple_nonce_cleanup', 'wp_snonce_cleanup' );
 }
 
-add_action( 'wp', 'wp_simple_nonce_register_garbage_collection' );
+if( function_exists( 'wp_simple_nonce_register_garbage_collection' ) ) {
+	function wp_simple_nonce_register_garbage_collection() {
+		if ( ! wp_next_scheduled( 'wp_simple_nonce_cleanup' ) ) {
+			wp_schedule_event( time(), 'daily', 'wp_simple_nonce_cleanup' );
+		}
+	}
+
+	add_action( 'wp', 'wp_simple_nonce_register_garbage_collection' );
+}
